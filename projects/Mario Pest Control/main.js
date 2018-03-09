47dead852782bfc8e9fb66ccf05e5832f3de3bce
tirzahe <tirzah410@gmail.com>
2018-03-08 21:42:09 -0700

    saving progress on Mario project

--- projects/Mario Pest Control/main.js
+++ projects/Mario Pest Control/main.js
@@ -1,11 +1,27 @@
 
 var totalGoombas = document.getElementById("goombas-caught");
-totalGoombas.addEventListener("input", sumGoombas);
+totalGoombas.addEventListener("input", baddySubtotal);
 // //create a function that calculates subtotal for each baddy based on total caught and price per baddy.
-function sumBaddy(event){
-    var price = 5;
-    var subtotalG = event.target.value * price;
+//identify Baddy
+//identify how many of that baddy were caught
+//identify price per baddy
+//calculate baddy's subtotal
+
+function baddySubtotal(event){
+    var target = totalGoombas;
+    var subtotal = event.target.value * price;
+    if(event.target === totalGoombas){
+        var price = 5;
+        totalGoombas.innerHTML = subtotal;
+    }
+    // }else if (event.target === /*Bob-ombs*/){
+    //     var price = 7;
+    // }else if (event.target === /*Cheep-cheeps*/){
+    //     var price = 11;
+    // }
+    
+    return subtotal;
 }
-// console.log(sumGoombas());
+console.log(baddySubtotal());
 
 //create a function that sums price of baddies caught.
