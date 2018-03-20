4ca46f6ba959522c25febc32daa386cd81d922ee
tirzahe <tirzah410@gmail.com>
2018-03-20 17:45:29 -0600

    submitting AxiosTodo Silver minus image and Axios Gold Level

--- projects/Axiostodo/script.js
+++ projects/Axiostodo/script.js
@@ -3,51 +3,81 @@ document.onload = getTodos();
 let todoList = document.getElementById("toDos");
 let form = document.getElementById("createTodoList");
 
-var inputData = {};
-//create newItem object
-    // 
-// 
-
 function domStuff(todoItem){
+    var id = todoItem._id;
     let listItem = document.createElement("div");
     let title = document.createElement("p");
     let description = document.createElement("p");
     let price = document.createElement("p");
+    // let image = document.createElement("input");
+    // image.type = 
     let checkBox = document.createElement("input");
     checkBox.type = "checkbox";
+    checkBox.checked = todoItem.completed;
+        if(checkBox.checked){
+            title.style.textDecoration = "line-through";
+            description.style.textDecoration = "line-through";
+        }
+    checkBox.addEventListener("change", function(event){
+        var id = todoItem._id;
+        var ckIt = event.target.checked;
+        axios.put("https://api.vschool.io/tirzah/todo/" + id, {completed: ckIt }).then(function (response){
+            if(ckIt){
+                title.style.textDecoration = "line-through";
+                description.style.textDecoration = "line-through";
+                price.style.textDecoration = "line-through";
+            } else{
+                title.style.textDecoration = "none";
+                description.style.textDecoration = "none";
+                price.style.textDecoration = "none";
+            }
+        }) 
+    });
+
+    let deleteButton = document.createElement("button");
+    deleteButton.type = "button";
+    deleteButton.innerHTML = "Delete";
+    deleteButton.addEventListener("click", function(event){
+        todoList.removeChild(listItem);
+        axios.delete("https://api.vschool.io/tirzah/todo/" + id, {id: event.target.value}).then(function (response){
+        })
+    });
+        
     
     title.innerHTML = todoItem.title;
-    description.innerHTML = todoItem.description;
-    price.innerHTML = todoItem.price;
+    description.innerHTML = "Description: " + todoItem.description;
+    price.innerHTML = "Price: $" + todoItem.price;
+    
     listItem.appendChild(title);
     listItem.appendChild(description);
     listItem.appendChild(price);
     listItem.appendChild(checkBox);
+    listItem.appendChild(deleteButton);
     todoList.appendChild(listItem);
 }
+
 function getTodos(){
     axios.get("https://api.vschool.io/tirzah/todo").then(function (response){
         var data = response.data;
-            for (var i = 0; i < data.length; i++) {
-                domStuff(data[i]);
-            }
-        });
+        for (var i = 0; i < data.length; i++) {
+            domStuff(data[i]);
+        }   
+    });     
 }   
-
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     var inputData = { 
-                title: form.title.value,
-                description: form.description.value,
-                price: form.price.value,
-                completed: false
-            };
+        title: form.title.value,
+        description: form.description.value,
+        price: form.price.value,
+        completed: false
+    };
+
+    domStuff(inputData);
     
     axios.post("https://api.vschool.io/tirzah/todo", inputData)
     .then(function (response) {
-        getTodos();
-
     })
     .catch(error => {
         console.error(error);
@@ -56,28 +86,4 @@ form.addEventListener("submit", function (event) {
     form.price.value = "";
     form.title.value = "";
 });
-
-// function setItemTitle(event){
-//     newItem.title = event.target.value;   
-// }
-// function setItemDescription(event){
-//     newItem.description = event.target.value;
-// }
-// function setItemPrice(event){
-//     newItem.price = event.target.value;
-// }
-
-
-// newTitle.addEventListener("input", setItemTitle);
-// newTitle.addEventListener("input", setItemDescription);
-// newTitle.addEventListener("input", setItemPrice);
-
-
-
-// axios.delete("https://api.vschool.io/tirzah/todo/id").then(function (response) {
-    
-//     for (var i = 0; i < response.data.length; i++) {
-        
-//     }
-// });
 
