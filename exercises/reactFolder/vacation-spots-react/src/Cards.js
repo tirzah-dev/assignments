16a05fd262a49155d10b2544de66231b08172e3c
tirzahe <tirzah410@gmail.com>
2018-04-03 18:51:56 -0600

    svg small fix to vacation cardslayout adjusting if statement to switch

--- exercises/reactFolder/vacation-spots-react/src/Cards.js
+++ exercises/reactFolder/vacation-spots-react/src/Cards.js
@@ -4,23 +4,30 @@ const Cards = props =>
     <li className="card">
         <h2>{props.vacation.place}</h2>
         <p>Price: ${props.vacation.price}</p>
-        {/* if (props.vacation.price < 500){
-            <h4>$</h4>
-        } else if (props.vacation.price >= 500 && props.vacation.price < 1000){
-            <h4>$$</h4>
-        }else{
-            <h4>$$$</h4>
-        } */}
-        {/* if (props.vacation.timeToGo === Spring){
-            style: "backgroundColor": "Green"
-        } else if (props.vacation.timeToGo === fall){
-            style: "backgroundColor": "PaleOrange" 
-        } else if (props.vacation.timeToGo === Summer){
-            style: "backgroundColor": "PaleYellow" 
-        }else (props.vacation.timeToGo === Winter){
-            style: "backgroundColor": "White" 
-        }*/}
+            {/* switch(props.vacation.price){
+                case > 1000:
+                    <h4>$$$</h4>
+                    break;
+                case =< 500:
+                    <h4>$</h4>
+                    break;
+                default:
+                    <h4>$$</h4>
+            } */}
+        
         <p>Best Season:  {props.vacation.timeToGo}</p>
+            {/* switch(props.vacation.timeToGo) {
+                case Spring:
+                {style: "backgroundColor": "#99C24D"}
+                break;
+                case Fall:
+                {style: "backgroundColor": "#F2B259"}
+                break;
+                case Summer:
+                {style: "backgroundColor": "#F5FF90"}
+                default:
+                {style: "backgroundColor": "#FFFFFF"}
+            }     */}
     </li>
 
 export default Cards;
