e46934fa4c76aacd539730fe5c6eba953dc42c5c
tirzahe <tirzah410@gmail.com>
2018-04-05 13:52:04 -0600

    submit Redux Contacts exercise

---
+++ exercises/reduxexercises/contactsPureJS/index.js
@@ -0,0 +1,52 @@
+const redux = require("redux");
+
+const initialState = {
+    contacts: [
+    {
+        name: "abcde",
+        phone: "888-299-0983"
+    },
+    {
+        name: "Jerry",
+        phone: "802-453-2346"
+    }
+    ]
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case "ADD_CONTACT":
+            return {
+                contacts: [...state.contacts, action.contact]
+            }
+        case "REMOVE_CONTACT":
+            return {
+                contacts: state.contacts.filter(contact => contact.name !== action.name)
+            }
+        default:
+            return state;
+    }
+}
+
+const store = redux.createStore(reducer);
+store.subscribe(() => console.log(store.getState()));
+
+//action is errand boy
+const addContact = contact => {
+    store.dispatch({
+        type: "ADD_CONTACT",
+        contact
+    })
+}
+const removeContact = name => {
+    store.dispatch({
+        type: "REMOVE_CONTACT",
+        name
+    })
+}
+addContact({
+        name: "ana",
+        phone: "883-979-0983"
+    });
+
+// removeContact("abcde")
